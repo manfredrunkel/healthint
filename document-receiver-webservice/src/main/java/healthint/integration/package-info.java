bdadbd1643280446dafe7e4a205dccd80523cc86
Manfred <manfred@kugel.com.br>
2016-08-18 21:46:05 -0300

    Added Parent POM

--- document-receiver-webservice/src/main/java/healthint/integration/package-info.java
+++ document-receiver-webservice/src/main/java/healthint/integration/package-info.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2016.08.03 às 12:54:21 PM BRT 
+// Gerado em: 2016.08.18 às 08:57:26 PM BRT 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://healthint/integration", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
